07e1d997a68e0b814b504b2beba3e9a3f0361fdd
Ajk665 <72042993+Ajk665@users.noreply.github.com>
2020-11-12 20:06:41 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var dog,happyDog;
+var database;
+var foodS,foodStock;
+
+function preload(){
+
+  happyD = loadImage("images/dogImg1.png");
+  Dog = loadImage("images/dogImg.png");
+
+}
+
+function setup() {
+  createCanvas(500, 500);
+  database = firebase.database();
+  dog = createSprite(250,250,50,50);
+  dog.scale = 0.1;
+  dog.addImage(Dog);
+  foodStock = database.ref('Food')
+  foodStock.on("value",readStock);
+}
+
+
+function draw() {  
+  background(46,139,87);
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyD);
+  }
+  drawSprites();
+  fill("black")
+  text("PRESS UP ARROW TO FEED DOG",200,50)
+  text("Food Remaining "+foodS,200,100);
+
+}
+function readStock(data){
+
+  foodS = data.val();
+
+}
+function writeStock(x){
+
+  if(x <= 0){
+
+    x = 0;
+
+  }else{
+
+    x = x-1
+
+  }
+  database.ref('/').update({
+    Food:x
+  })
+
+}
+
+
